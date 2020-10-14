13a47167de34130a6d4cc76e665d696ea4bd4936
Kevin Potter <kevin.w.potter@gmail.com>
2020-10-14 18:09:10 -0400

    feat: Updated functions for diagram and nodes and connectors

---
+++ R/S01-create_base_figure.R
@@ -0,0 +1,151 @@
+#' Create Base Figure for Diagram
+#'
+#' A function that generates a base figure,
+#' with optional guidelines, for a diagram.
+#'
+#' @param default Pre-packaged figure sizes, including...
+#'   \itemize{
+#'     \item 'US letter' (11 x 8.5 inches);
+#'     \item '4.6' (4.6 x 4.6 inches);
+#'     \item '9.2 x 4.6' (in inches).
+#'   }
+#' @param w Width (in inches) for the x-axis.
+#' @param h Height (in inches) for the y-axis.
+#' @param orientation For default specifications, indicates
+#'   whether figures should be in \code{landscape} or
+#'   \code{portrait} style.
+#' @param margin A vector with the margins (in inches) for
+#'   the bottom, left, top, and right, respectively.
+#' @param guidelines Logical; if \code{TRUE}, includes
+#'   guidelines when generating the figure.
+#' @param guide_positions Vector giving the guideline
+#'   positions (values must be between 0 and 1).
+#' @param guide_adjust An adjustment controlling the
+#'   position on the axes for the guideline numbers.
+#' @param new Logical; if \code{TRUE} a new plotting
+#'   window is generated.
+#'
+#' @examples
+#' # Default (6 x 6 inches)
+#' create_base_figure()
+#'
+#' # No guidelines
+#' create_base_figure( guidelines = FALSE )
+#'
+#' # US letter size
+#' create_base_figure( default = 'US letter' )
+#'
+#' # US letter (Portrait)
+#' create_base_figure( default = 'US letter', orientation = 'portrait' )
+#'
+#' @export
+
+create_base_figure = function( default = NULL,
+                               w = 6, h = 6,
+                               orientation = 'landscape',
+                               margin = rep( .25, 4 ),
+                               guidelines = T,
+                               guide_positions = seq( .1, .9, .1 ),
+                               guide_adjust = 3/4,
+                               new = T ) {
+
+  if ( !is.null( default ) ) {
+
+    if ( default %in% 'US letter' ) {
+      if ( orientation %in% c( 'Landscape', 'landscape', 'wide', 'Wide' ) ) {
+        w = 11
+        h = 8.5
+      }
+      if ( orientation %in% c( 'Portrait', 'portrait', 'Tall', 'tall' ) ) {
+        w = 8.5
+        h = 11
+      }
+    }
+
+    if ( default %in% c( '4.6 x 4.6 in', '4.6' ) ) {
+      w = 4.6
+      h = 4.6
+    }
+
+    if ( default %in% '9.2 x 4.6' ) {
+      if ( orientation %in% c( 'Landscape', 'landscape', 'wide', 'Wide' ) ) {
+        w = 9.2
+        h = 4.6
+      }
+      if ( orientation %in% c( 'Portrait', 'portrait', 'Tall', 'tall' ) ) {
+        w = 4.6
+        h = 9.2
+      }
+    }
+
+  }
+
+  # Create plotting window
+  if ( new ) x11( width = w, height = h )
+
+  # Specify margins
+  par( mai = margin )
+
+  # Create blank plot
+  plot(
+    c( 0, 1 ), c( 0, 1 ),
+    xaxt = 'n', yaxt = 'n',
+    xlab = '', ylab = '',
+    bty = 'n', type = 'n'
+  )
+
+  if ( guidelines ) {
+
+    plot_range = list(
+      start = rep( 0, length( guide_positions ) ),
+      end = rep( 1, length( guide_positions ) )
+    )
+
+    # Grid lines
+    segments(  guide_positions,
+               plot_range$start,
+               guide_positions,
+               plot_range$end,
+               col = 'grey95' )
+    segments(  plot_range$start,
+               guide_positions,
+               plot_range$end,
+               guide_positions,
+               col = 'grey95' )
+
+    # Outer margins
+    abline( v = 0, xpd = NA )
+    abline( h = 0, xpd = NA )
+    abline( v = 1, xpd = NA )
+    abline( h = 1, xpd = NA )
+
+    # Plotting dimensions
+
+    # Plot window
+    dm_outer = dev.size(); names( dm_outer ) = c( 'x', 'y' )
+    # Margins
+    dm_margins = par( 'mai' )
+    # Plotting window
+    dm_inner = c(
+      dm_outer[1] - dm_margins[2] - dm_margins[4],
+      dm_outer[2] - dm_margins[1] - dm_margins[3]
+    )
+
+    text( guide_positions,
+          rep( ( dm_outer[2] - guide_adjust * dm_margins[3] ) / dm_inner[2],
+               length( guide_positions ) ),
+          guide_positions,
+          xpd = NA )
+
+    text( rep( 1 - ( dm_outer[1] - guide_adjust * dm_margins[2] ) / dm_inner[1],
+               length( guide_positions ) ),
+          guide_positions,
+          guide_positions,
+          xpd = NA )
+
+  }
+
+  # Reset margins
+  par( mar = c( 5, 4, 4, 2 ) + .1 )
+}
+
---
+++ R/S04-add_node.R
@@ -0,0 +1,243 @@
+#' Add a Node to a Diagram
+#'
+#' Adds a node to an existing diagram (see
+#' \code{\link{create_base_figure}}.
+#'
+#' @param content A list governing the position,
+#'   appearance, and content of the node (see the
+#'   helper function \code{\link{node_content}}).
+#' @param check_for_style_guide Logical; if \code{TRUE},
+#'   checks if a list called \code{style_guide}
+#'   exists in the global environment; if so,
+#'   default options for the node generation
+#'   are based on the contents of this list.
+#' @param ... Additional arguments for the
+#'   \code{\link[graphics]{polygon}} function.
+#'
+#' @return A named list giving the x and y-axis coordinates for
+#'   the \code{center}, \code{left}, \code{right}, \code{top},
+#'   \code{bottom}, \code{topleft}, \code{bottomleft},
+#'   \code{topright}, and \code{bottomright}.
+#'
+#' @examples
+#' # Create diagram
+#' create_base_figure( guidelines = F )
+#' # Add node
+#' nd = add_node( node_content( x = .5, y = .9, text = 'Hello world' ) )
+#'
+#' @export
+
+add_node = function( content,
+                     check_for_style_guide = T,
+                     ... ) {
+
+  # 1) Extract content elements
+
+  x = content$x; y = content$y
+  txt = content$text
+  dmn = content$dimensions
+  shape = content$shape
+  spacing = content$spacing
+  border_color = content$border_color
+  node_color = content$node_color
+  text_size = content$text_size
+  text_color = content$text_color
+  alignment = content$alignment
+
+  # 2) Defaults
+
+
+  # Based on style guide
+  if ( check_for_style_guide ) {
+
+    if ( exists( 'style_guide', envir = .GlobalEnv ) ) {
+
+      if ( is.null( shape ) )
+        shape = style_guide$node$shape
+
+      if ( is.null( spacing ) )
+        spacing = style_guide$node$spacing
+
+      if ( is.null( border_color ) )
+        border_color = style_guide$node$border_color
+
+      if ( is.null( node_color ) )
+        node_color = style_guide$node$node_color
+
+      if ( is.null( text_size ) )
+        text_size = style_guide$node$text_size
+
+      if ( is.null( text_color ) )
+        text_color = style_guide$node$text_color
+
+      if ( is.null( alignment ) )
+        alignment = style_guide$node$alignment
+
+    }
+
+  }
+
+  # Based on internal function options
+
+  if ( is.null( shape ) )
+    shape = 'rectangle'
+
+  if ( is.null( spacing ) )
+    spacing = 0.5
+
+  if ( is.null( border_color ) )
+    border_color = 'black'
+
+  if ( is.null( node_color ) )
+    node_color = 'white'
+
+  if ( is.null( text_size ) )
+    text_size = 1
+
+  if ( is.null( text_color ) )
+    text_color = 'black'
+
+  if ( is.null( alignment ) )
+    alignment = 'center'
+
+  # 3) Text content
+
+  # Example construction
+  #        | left | special | right | font
+  # Line 1 | ''   | 'Hello' | ''    | 2
+  # Line 2 | '  ' | 'World' | '!'   | 3
+
+  # Set text size
+  par( cex = text_size )
+
+  # In a character vector is provided, convert to list format
+  if ( is.character( txt ) ) {
+
+    n_lines = length( txt )
+    txt = list(
+      left = txt,
+      special = rep( '', n_lines ),
+      right = rep( '', n_lines ),
+      font = rep( 1, n_lines )
+    )
+
+  }
+
+  if ( is.list( txt ) ) {
+
+    if ( all( names( txt ) %in% c( 'left', 'special', 'right', 'font' ) ) ) {
+
+      # Determine number of lines for text to add
+      n_lines = length( txt$right )
+      # Height of text
+      h = max( sapply( txt$right, strheight ) )
+      # Width of text based on type
+      w = matrix( NA, n_lines, 3 )
+      colnames( w ) = c( 'left', 'special', 'right' )
+
+      w[,'left'] = sapply( txt$left, strwidth )
+      w[,'special'] = sapply( txt$special, strwidth )
+      w[,'right'] = sapply( txt$right, strwidth )
+
+    }
+
+  } else {
+    stop( '' )
+  }
+
+  # spacing
+  # 1. Line 1 ~
+  #    spacing
+  # 2. Line 2 ~
+  # spacing
+
+  # Total height and width based on text size
+  h_space = h * spacing
+  h.total = h * n_lines + ( n_lines + 1) * h_space
+  w.total = max( rowSums( w ) )
+
+  # 4) Add background shape
+
+  # By default, size of shape is based on
+  # text
+  if ( is.null( dmn ) ) {
+    dmn = c(
+      w.total + h * spacing,
+      h.total
+    )
+  }
+
+  if ( shape == "rectangle" ) {
+
+    xa = x + dmn[1] * c(-.5, -.5, .5, .5)
+    ya = y + dmn[2] * c(-1, 0, 0, -1)
+    polygon( xa, ya,
+             col = node_color,
+             border = border_color,
+             ... )
+
+  }
+
+  # 5) Add text
+
+  # Determine position for x-axis
+  if ( alignment == 'center' ) {
+    x.pos = x - rowSums( w )/2
+  }
+  if ( alignment == 'left' ) {
+    x.pos = rep( x - w.total/2, n_lines )
+  }
+  positioning = 4
+
+  # Determine position for y-axis
+  y.pos = seq( y - h_space - h/2,
+               ( y - h.total ) + h_space + h/2,
+               length.out = n_lines )
+  # y.pos = seq( y, y - h.total, length.out = n_lines + 2 )
+  # y.pos = y.pos[ -c( 1, length( y.pos ) ) ]
+
+  # Add text
+  for ( i in 1:n_lines ) {
+
+    if ( w[i,1] > 0 ) {
+      text( x.pos[i], y.pos[i],
+            txt$left[i],
+            pos = positioning, offset = 0 )
+    }
+    if ( w[i,2] > 0 ) {
+      text( x.pos[i] + w[i,1], y.pos[i],
+            txt$special[i],
+            col = text_color,
+            pos = positioning, offset = 0, font = txt$font[i] )
+    }
+    if ( w[i,3] > 0 ) {
+      text( x.pos[i] + sum( w[i,1:2] ), y.pos[i],
+            txt$right[i],
+            col = text_color,
+            pos = positioning, offset = 0 )
+    }
+
+  }
+
+  x.c = x; y.c = y - dmn[2]/2
+  w2 = dmn[1]/2
+  h2 = dmn[2]/2
+
+  # Reset text size
+  par( cex = 1 )
+
+  out = list(
+    center = c( x.c, y.c ),
+    left = c(x.c - w2, y.c),
+    right = c(x.c + w2, y.c),
+    top = c(x.c, y.c + h2),
+    bottom = c(x.c, y.c - h2),
+    topleft = c(x.c - w2, y.c + h2),
+    bottomleft = c(x.c - w2, y.c - h2),
+    topright = c(x.c + w2, y.c + h2),
+    bottomright = c(x.c + w2, y.c - h2)
+  )
+
+  return(out)
+}
+
