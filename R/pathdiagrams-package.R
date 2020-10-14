ed982c3a9ef460c53588aa7d6ffb074f3e98ea76
Kevin Potter <kevin.w.potter@gmail.com>
2020-10-14 10:15:15 -0400

    feat: Initial commit

---
+++ R/pathdiagrams-package.R
@@ -0,0 +1,25 @@
+# Description for the 'pathdiagrams' package
+# Written by Kevin Potter
+# email: kevin.w.potter@gmail.com
+# Please email me directly if you
+# have any questions or comments
+# Last updated 2019-10-14
+
+# Package development
+# library(devtools)
+# library(roxygen2)
+
+#' Tools to Create Path Diagrams
+#'
+#' @docType package
+#' @name pathdiagrams-package
+#'
+#' @description
+#'
+#' The \pkg{pathdiagrams} package provides a set of
+#' functions to streamline creation of path diagrams
+#' and flow charts. A complete  list of the
+#' functions available in the package can be obtained
+#' via the command \code{ls(pos = "package:pathdiagrams")}.
+#'
+NULL
